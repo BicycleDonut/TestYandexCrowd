8fdd41f6d85387312c6162c952d1ee62099a9772
Alexander Ryzhov <aryzhov84@gmail.com>
2023-11-05 15:30:30 +0300

    Add HW3

---
+++ HW3/src/seminarThreeHW/exceptions/ProductDatabaseException.java
@@ -0,0 +1,8 @@
+package seminarThreeHW.exceptions;
+
+public class ProductDatabaseException extends Exception {
+
+    public ProductDatabaseException(String message) {
+        super(message);
+    }
+}
---
+++ HW3/src/seminarThreeHW/exceptions/ShopManagerException.java
@@ -0,0 +1,8 @@
+package seminarThreeHW.exceptions;
+
+public class  ShopManagerException extends Exception {
+
+    public ShopManagerException(String message) {
+        super(message);
+    }
+}
